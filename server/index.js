d302bc803df4c7d7249eeaef3e9c13d3a6a1dcf6
Wei Wang <wang0109@gmail.com>
2016-01-12 08:56:13 -0800

    add routes: site is up

--- server/index.js
+++ server/index.js
@@ -58,8 +58,42 @@ app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
 //===============ROUTES===============
+//
 
 //This section will hold our Routes
+//===============ROUTES=================
+//displays our homepage
+app.get('/', function(req, res){
+  res.render('home', {user: req.user});
+});
+
+//displays our signup page
+app.get('/signin', function(req, res){
+  res.render('signin');
+});
+
+//sends the request through our local signup strategy, and if successful takes user to homepage, otherwise returns then to signin page
+app.post('/local-reg', passport.authenticate('local-signup', {
+  successRedirect: '/',
+  failureRedirect: '/signin'
+  })
+);
+
+//sends the request through our local login/signin strategy, and if successful takes user to homepage, otherwise returns then to signin page
+app.post('/login', passport.authenticate('local-signin', { 
+  successRedirect: '/',
+  failureRedirect: '/signin'
+  })
+);
+
+//logs user out of site, deleting them from the session, and returns to homepage
+app.get('/logout', function(req, res){
+  var name = req.user.username;
+  console.log("LOGGIN OUT " + req.user.username)
+  req.logout();
+  res.redirect('/');
+  req.session.notice = "You have successfully been logged out " + name + "!";
+});
 
 //===============PORT=================
 var port = process.env.PORT || 5000; //select your port or let it pull from your .env file
